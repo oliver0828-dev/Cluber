9d5a6c3104d9673b49158b2b9967a99a1dadd7fc
Oliver Park <oliverco0828@gmail.com>
2024-04-23 21:32:55 +0900

    Initial Commit

---
+++ Cluber/CluberApp.swift
@@ -0,0 +1,17 @@
+//
+//  CluberApp.swift
+//  Cluber
+//
+//  Created by Oliver Park on 4/23/24.
+//
+
+import SwiftUI
+
+@main
+struct CluberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
