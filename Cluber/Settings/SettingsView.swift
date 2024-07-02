8b1320197dda5a7bb610c3d35b1c25b49e3d569f
Oliver Park <oliverco0828@gmail.com>
2024-07-02 14:24:19 +0900

    edit setting

--- Cluber/Settings/SettingsView.swift
+++ Cluber/Settings/SettingsView.swift
@@ -48,6 +48,7 @@ struct SettingsView: View {
                       
                   }
               }
+              Text("ADD")
             }
             
           .navigationTitle("Settings")
