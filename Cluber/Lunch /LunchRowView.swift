c1dce5645cc52a507f303755f91d49a168d3df58
Oliver Park <oliverco0828@gmail.com>
2024-07-19 19:54:04 +0900

    update

---
+++ Cluber/Lunch /LunchRowView.swift
@@ -0,0 +1,18 @@
+//
+//  LunchRowView.swift
+//  Cluber
+//
+//  Created by Oliver Park on 7/19/24.
+//
+
+import SwiftUI
+
+struct LunchRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LunchRowView()
+}
