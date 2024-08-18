9b049ab74fe75a64b6567a23ab102494099135f4
Oliver Park <oliverco0828@gmail.com>
2024-08-18 16:29:58 +0900

    IG Update

--- Cluber/IGShareView.swift
+++ Cluber/IGShareView.swift
@@ -10,13 +10,12 @@ import SwiftUI
 struct IGShareView: View {
     @EnvironmentObject var usernameGrade: UsernameGradeClass
     @Environment(\.colorScheme) var colorScheme
+    
     @State private var highSchoolLunchClubList: [LunchClubStruct] = LunchClubList.HighSchool
     @State private var elementarySchoolLunchClubList: [LunchClubStruct] = LunchClubList.ElementarySchool
     @State private var middleSchoolLunchClubList: [LunchClubStruct] = LunchClubList.MiddleSchool
-    
-    @State private var instagramButton = false
-    
     @State private var clubCount = 0
+    @State private var image: UIImage?
     
     var body: some View {
         GeometryReader { _ in
@@ -24,6 +23,7 @@ struct IGShareView: View {
                 Image("igShareImage")
                     .frame(maxWidth: 200)
                     .ignoresSafeArea()
+                
                 VStack {
                     VStack(alignment: .center) {
                         HStack {
@@ -36,7 +36,7 @@ struct IGShareView: View {
                         .font(.title)
                         .fontWeight(.medium)
                         .padding()
-
+                        
                         if usernameGrade.schoolGrade == "Elementary" {
                             Image("gliderYellow")
                                 .resizable()
@@ -53,14 +53,14 @@ struct IGShareView: View {
                                 .scaledToFit()
                                 .frame(width: 250, height: 250)
                         }
-
+                        
                         Text(usernameGrade.userName)
                             .font(.title.bold())
                             .frame(width: 140, height: 40)
                             .background(.gray.opacity(0.4))
                             .clipShape(.capsule)
                             .padding()
-
+                        
                         HStack(spacing: 1) {
                             ForEach(1...5, id: \.self) { day in
                                 VStack {
@@ -69,7 +69,7 @@ struct IGShareView: View {
                                         .background(Color(red: 0, green: 0.51, blue: 0.6).opacity(0.35))
                                         .clipShape(Capsule())
                                         .foregroundColor(.white)
-
+                                    
                                     switch usernameGrade.schoolGrade {
                                     case "Elementary":
                                         let clubsForDay = elementarySchoolLunchClubList.filter { $0.dayOfWeek == day && $0.isMember }
@@ -88,6 +88,7 @@ struct IGShareView: View {
                                                     .clipShape(RoundedRectangle(cornerRadius: 12))
                                             }
                                         }
+                                        
                                     case "Middle":
                                         let clubsForDay = middleSchoolLunchClubList.filter { $0.dayOfWeek == day && $0.isMember }
                                         if clubsForDay.isEmpty {
@@ -105,6 +106,7 @@ struct IGShareView: View {
                                                     .clipShape(RoundedRectangle(cornerRadius: 12))
                                             }
                                         }
+                                        
                                     case "High":
                                         let clubsForDay = highSchoolLunchClubList.filter { $0.dayOfWeek == day && $0.isMember }
                                         if clubsForDay.isEmpty {
@@ -122,6 +124,7 @@ struct IGShareView: View {
                                                     .clipShape(RoundedRectangle(cornerRadius: 12))
                                             }
                                         }
+                                        
                                     default:
                                         Text("Choose Your Lunch Club")
                                     }
@@ -129,18 +132,42 @@ struct IGShareView: View {
                             }
                         }
                         .foregroundStyle(.black)
+                        
+                        Button {
+                            image = takeCapture()
+                        } label: {
+                            Text("Capture")
+                        }
+                        
+                        if let image {
+                            Button {
+                                instagramShare(image: image)
+                            } label: {
+                                HStack {
+                                    Image("instagram")
+                                        .resizable()
+                                        .scaledToFit()
+                                        .frame(width: 20, height: 20)
+                                    Text("Instagram Stories")
+                                        .foregroundStyle(.white)
+                                }
+                                .frame(width: 200, height: 50)
+                                .background(.black.gradient)
+                                .clipShape(.rect(cornerRadius: 15))
+                            }
+                            Spacer()
+                        }
                     }
-                    Spacer()
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .onAppear {
+                        updateClubCount()
+                    }
+                    .onChange(of: usernameGrade.schoolGrade) { _ in
+                        updateClubCount()
+                    }
+                    .fontDesign(.rounded)
                 }
             }
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
-            .onAppear {
-                updateClubCount()
-            }
-            .onChange(of: usernameGrade.schoolGrade) { _ in
-                updateClubCount()
-            }
-            .fontDesign(.rounded)
         }
     }
     
@@ -154,6 +181,50 @@ struct IGShareView: View {
             clubCount = highSchoolLunchClubList.filter { $0.isMember }.count
         default:
             clubCount = 0
+        }
+    }
+    
+    func takeCapture() -> UIImage {
+        var image: UIImage?
+        guard let currentLayer = UIApplication.shared.windows.first { $0.isKeyWindow }?.layer else { return UIImage() }
+        
+        let currentScale = UIScreen.main.scale
+        
+        UIGraphicsBeginImageContextWithOptions(currentLayer.frame.size, false, currentScale)
+        guard let currentContext = UIGraphicsGetCurrentContext() else { return UIImage() }
+        currentLayer.render(in: currentContext)
+        
+        image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return image ?? UIImage()
+    }
+    
+    func instagramShare(image: UIImage) {
+        if let urlScheme = URL(string: "instagram-stories://share") {
+            if UIApplication.shared.canOpenURL(urlScheme) {
+                let pasteboardItems = [
+                    [
+                        "com.instagram.sharedSticker.stickerImage": image.pngData(),
+                        "com.instagram.sharedSticker.backgroundImage": image.pngData()
+                    ]
+                ]
+                
+                let pasteboardOptions = [UIPasteboard.OptionsKey.expirationDate: Date().addingTimeInterval(60 * 5)]
+                UIPasteboard.general.setItems(pasteboardItems, options: pasteboardOptions)
+                
+                UIApplication.shared.open(urlScheme as URL, options: [:], completionHandler: nil)
+            } else {
+                print("Instagram app is not installed.")
+            }
+        }
+    }
+    
+    struct Photo: Transferable {
+        public var image: Image
+        
+        static var transferRepresentation: some TransferRepresentation {
+            ProxyRepresentation(exporting: \.image)
         }
     }
 }
@@ -200,15 +271,18 @@ func CircleColor(gradeLevel: String, colorScheme: ColorScheme) -> Color {
 }
 
 func textConvert(num: Int) -> String {
-    if num == 1 {
+    switch num {
+    case 1:
         return "MON"
-    } else if num == 2 {
+    case 2:
         return "TUE"
-    } else if num == 3 {
+    case 3:
         return "WED"
-    } else if num == 4 {
+    case 4:
         return "THU"
-    } else {
+    case 5:
         return "FRI"
+    default:
+        return "Unknown"
     }
 }
