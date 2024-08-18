3ba8a9ad454d49b1bcfae2d58b2ed835f12d1284
정지혁 <cars0106@icloud.com>
2024-08-18 11:02:43 +0900

    페이스북 SDK 설정 및 초기화

--- Cluber/CluberApp.swift
+++ Cluber/CluberApp.swift
@@ -6,9 +6,41 @@
 //
 
 import SwiftUI
+import FacebookCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(
+        _ application: UIApplication,
+        configurationForConnecting connectingSceneSession: UISceneSession,
+        options: UIScene.ConnectionOptions
+    ) -> UISceneConfiguration {
+        let sceneConfig = UISceneConfiguration(name: nil, sessionRole: connectingSceneSession.role)
+        
+        sceneConfig.delegateClass = SceneDelegate.self
+        
+        return sceneConfig
+    }
+}
+
+class SceneDelegate: NSObject, UIWindowSceneDelegate {
+    func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) {
+        guard let url = URLContexts.first?.url else {
+            return
+        }
+        
+        ApplicationDelegate.shared.application(
+            UIApplication.shared,
+            open: url,
+            sourceApplication: nil,
+            annotation: [UIApplication.OpenURLOptionsKey.annotation]
+        )
+    }
+}
 
 @main
 struct CluberApp: App {
+    @UIApplicationDelegateAdaptor var delegate: AppDelegate
+    
     @StateObject var usernameGrade = UsernameGradeClass()
     @StateObject var photoImage = PhotoPickerViewModel()
     
